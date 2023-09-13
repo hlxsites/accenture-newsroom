82243e838281657f44c1cee5ab516c03c36f2e44
Amol Anand <amol.anand@gmail.com>
2023-09-13 18:19:45 -0400

    more caveats

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -205,7 +205,7 @@ export default {
 
     // add section after abstract
     const contentDetails = main.querySelector('#tek-wrap-centerwell article #content-details');
-    const abstractRegex = /(.*?);(.*?)(\d{4})|(.*?)(\d{4})\s+–\s+\b|(.*?)(\d{4})\s+-\s+\b|\b(\d+)\b(.*?)(\d{4})\b/;
+    const abstractRegex = /(.*?);(.*?)(\d{4})|(.*?)(\d{4})\s+–\s+\b|(.*?)(\d{4})\s+-\s+\b|\b(\d+)\b(.*?)(\d{4})\b|(.*?),(.*?)(\d{4})\b/;
     const contentDetailsTextNodes = [];
     collectTextNodes(contentDetails, contentDetailsTextNodes);
     const matchingParagraph = contentDetailsTextNodes.find(
