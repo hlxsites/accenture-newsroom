551970b1c7c62dac23d5f47e8bfc00230b891d86
Ravi Kiran Papani <rpapani@adobe.com>
2023-10-05 18:17:07 -0700

    fix: using vidyard api to load the video player ratehr than iframe

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,30 +1,26 @@
-import { readBlockConfig } from '../../scripts/lib-franklin.js';
+import { loadScript, readBlockConfig } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
+const VIDYARD_URL = 'https://play.vidyard.com/';
+const VIDYARD_SCRIPT_URL = 'https://play.vidyard.com/embed/v4.js';
+
+function getUUID(url) {
+  return new URL(url).pathname;
+}
+
+export default async function decorate(block) {
   const config = readBlockConfig(block);
-  if (config.url) {
-    const observer = new IntersectionObserver((entries) => {
-      if (entries.some((e) => e.isIntersecting)) {
-        const html = `
-          <div class="video-container">
-            <div>
-                <iframe loading="lazy" allow="encrypted-media" allowfullscreen="" src="${config.url}"></iframe>
-            </div>
-          </div>
-        `;
-        block.innerHTML = html;
-        observer.disconnect();
-      }
-    });
-    observer.observe(block);
+  if (config.url || config.uuid) {
+    await loadScript(VIDYARD_SCRIPT_URL, { async: 'true' });
     block.textContent = '';
-    const previewImage = config.preview;
-    if (previewImage) {
-      const img = document.createElement('img');
-      img.src = previewImage;
-      img.alt = config.title || '';
-      block.appendChild(img);
-    }
+    const uuid = config.uuid || getUUID(config.url);
+    const img = document.createElement('img');
+    img.src = `${VIDYARD_URL}${uuid}.jpg`;
+    img.alt = config.title || '';
+    img.setAttribute('data-uuid', uuid);
+    img.setAttribute('data-v', 4);
+    img.setAttribute('data-type', 'inline');
+    img.classList.add('vidyard-player-embed');
+    block.appendChild(img);
   } else {
     block.remove();
   }
