e185e8ad7bc8715aeb6efacf03a0453942374399
Amol Anand <amol.anand@gmail.com>
2023-11-27 16:06:53 -0500

    switching to mousedown

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -783,6 +783,18 @@ const preflightListener = async () => {
   customModal.showModal();
 };
 
+// eslint-disable-next-line consistent-return
+const publishListener = () => {
+  // eslint-disable-next-line no-restricted-globals, no-alert
+  if (confirm('Are you sure you want to publish this content live?')) {
+    // let the sidekick continue publishing
+    // eslint-disable-next-line no-console
+    console.debug('publish clicked');
+  } else {
+    // avoid publishing
+    return false;
+  }
+};
 /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
@@ -808,20 +820,11 @@ async function loadLazy(doc) {
   const sk = document.querySelector('helix-sidekick');
   if (sk) {
     sk.addEventListener('custom:preflight', preflightListener);
+    sk.addEventListener('publish', publishListener);
     const publishButtons = sk.querySelectorAll('button[title="Publish"]');
     publishButtons.forEach((publishButton) => {
       // eslint-disable-next-line consistent-return
-      publishButton.addEventListener('click', () => {
-        // eslint-disable-next-line no-restricted-globals, no-alert
-        if (confirm('Are you sure you want to publish this content live?')) {
-          // let the sidekick continue publishing
-          // eslint-disable-next-line no-console
-          console.debug('publish clicked');
-        } else {
-          // avoid publishing
-          return false;
-        }
-      });
+      publishButton.addEventListener('mousedown', publishListener);
     });
   }
 
