c4d70a271d9c0cbea30ba08b33e46939d5f9dac8
Ricaña <ricana.vee-jay@accenture.com>
2023-11-28 20:34:29 +0800

    Refactor: Implement key value for tags json

--- tools/tagger/tagger.js
+++ tools/tagger/tagger.js
@@ -1,42 +1,11 @@
-import {
-  fetchPlaceholders,
-} from '../../scripts/lib-franklin.js';
-
-function sanitizeName(name) {
-  if (!name) {
-    return '';
-  }
-
-  const decodedText = decodeURIComponent(name.trim()).toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  const sanitizedText = decodedText.split(',').map((text) => text.trim().replace(/(?<=[^\s/])\s*\/\s*(?=[^\s/])/g, '').replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '')).join(', ');
-  return sanitizedText.trim();
-}
-
-function getPlaceholder(key, placeholders) {
-  if (placeholders && placeholders[key]) {
-    return placeholders[key];
-  }
-  return key;
-}
-
-function toCamelCaseTag(tag) {
-  return tag.replace(/[^a-zA-Z0-9]/g, ' ').split(' ')
-    .map((word, index) => (index === 0 ? word.toLowerCase()
-      : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()))
-    .join('');
-}
-
-function renderItems(cat, catId, taxonomy, placeholders) {
+function renderItems(cat, catId, taxonomy) {
   let html = '';
-  const items = taxonomy.map((item) => item[cat]);
-  items.forEach((tag) => {
-    const replaceAndTag = tag === 'Strategy & Consulting' ? tag.replace('&', 'and') : tag;
-    const sToCamelCaseTag = toCamelCaseTag(replaceAndTag.replace(/(?<=\w)\/(?=\w)/g, '')); // 'Win/News' --> 'WinNews' ....... "Win / News" --> "Win / News"
-    const tagNamePlaceHolder = getPlaceholder(sToCamelCaseTag, placeholders);
-    if (tag.trim() !== '') {
+  const oCategoryTags = taxonomy[cat];
+  oCategoryTags.forEach((tag) => {
+    if (tag.key.trim() !== '' && tag.text.trim() !== '') {
       html += `
       <span class="path">
-        <span data-title="${replaceAndTag}" class="tag cat-${catId % 8}">${tagNamePlaceHolder}</span>
+        <span data-title="${tag.key}" class="tag cat-${catId % 8}">${tag.text}</span>
       </span>
     `;
     }
@@ -44,20 +13,39 @@ function renderItems(cat, catId, taxonomy, placeholders) {
   return html;
 }
 
-function initTaxonomy(taxonomy, placeholders) {
+const getReduceTags = (oTaxonomy, sCategory) => {
+  const sCategoryCapital = sCategory.charAt(0).toUpperCase() + sCategory.slice(1);
+
+  return oTaxonomy.reduce((oAccumulated, oObject) => {
+    oAccumulated[sCategory] || (oAccumulated[sCategory] = []); 
+    oAccumulated[sCategory].push({
+       key: oObject[`${sCategoryCapital} Key`],
+       text: oObject[`${sCategoryCapital} Text`]
+     });
+    return oAccumulated;
+  }, {});
+};
+
+function initTaxonomy(taxonomy) {
   let html = '';
-  Object.keys(taxonomy[0]).forEach((cat, idx) => {
+  const subjectsTags = getReduceTags(taxonomy, 'subjects');
+  const industriesTags = getReduceTags(taxonomy, 'industries');
+  const tagsJson = {...subjectsTags, ...industriesTags};
+
+  Object.keys(tagsJson).forEach((cat, idx) => {
+    const sCategory = cat.charAt(0).toUpperCase() + cat.slice(1);
     html += '<div class="category">';
-    html += `<h2>${cat}</h2>`;
-    html += renderItems(cat, idx, taxonomy, placeholders);
+    html += `<h2>${sCategory}</h2>`;
+    html += renderItems(cat, idx, tagsJson);
     html += '</div>';
   });
+
   const results = document.getElementById('results');
   results.innerHTML = html;
 }
 
 async function getTaxonomy() {
-  const resp = await fetch('/tags.json');
+  const resp = await fetch('/tags-placeholders.json');
   const tagsJson = await resp.json();
   return tagsJson.data;
 }
@@ -65,12 +53,12 @@ async function getTaxonomy() {
 function filter() {
   const searchTerm = document.getElementById('search').value.toLowerCase();
   document.querySelectorAll('#results .tag').forEach((tag) => {
-    const { title } = tag.dataset;
-    const offset = title.toLowerCase().indexOf(searchTerm);
+    const tagText = tag.innerText;
+    const offset = tagText.toLowerCase().indexOf(searchTerm);
     if (offset >= 0) {
-      const before = title.substring(0, offset);
-      const term = title.substring(offset, offset + searchTerm.length);
-      const after = title.substring(offset + searchTerm.length);
+      const before = tagText.substring(0, offset);
+      const term = tagText.substring(offset, offset + searchTerm.length);
+      const after = tagText.substring(offset + searchTerm.length);
       tag.innerHTML = `${before}<span class="highlight">${term}</span>${after}`;
       tag.closest('.path').classList.remove('filtered');
     } else {
@@ -95,12 +83,12 @@ function displaySelected() {
 
   selTagsEl.innerHTML = '';
   const selectedTags = document.querySelectorAll('#results .path.selected');
+ 
   if (selectedTags.length > 0) {
     selectedTags.forEach((path) => {
       const clone = path.cloneNode(true);
       clone.classList.remove('filtered', 'selected');
       const tag = clone.querySelector('.tag');
-      tag.innerHTML = tag.dataset.title;
       clone.addEventListener('click', () => {
         toggleTag(path);
       });
@@ -119,16 +107,14 @@ function displaySelected() {
 
 async function init() {
   const tax = await getTaxonomy();
-  const placeholders = await fetchPlaceholders();
 
-  initTaxonomy(tax, placeholders);
+  initTaxonomy(tax);
 
   const selEl = document.getElementById('selected');
   const copyButton = selEl.querySelector('button.copy');
   copyButton.addEventListener('click', () => {
     const copyText = document.getElementById('copybuffer');
-    const sanitizedText = sanitizeName(copyText.value);
-    navigator.clipboard.writeText(sanitizedText);
+    navigator.clipboard.writeText(copyText.value);
 
     copyButton.disabled = true;
   });
