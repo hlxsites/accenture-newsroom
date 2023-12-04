8fe5ba70233faad2887f278d5d3fb32fa573cfbc
Amol Anand <amol.anand@gmail.com>
2023-12-04 12:39:08 -0500

    Publish Later form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -783,6 +783,21 @@ const preflightListener = async () => {
   customModal.showModal();
 };
 
+const publishLaterListener = async () => {
+  const section = createTag('div');
+  const wrapper = createTag('div');
+  section.appendChild(wrapper);
+  const plBlock = buildBlock('form', `${window.hlx.codeBasePath}/drafts/amol/publish-later.json`);
+  wrapper.appendChild(plBlock);
+  decorateBlock(plBlock);
+  await loadBlock(plBlock);
+  const { default: getModal } = await import('../blocks/modal/modal.js');
+  const customModal = await getModal('dialog-modal', () => section.innerHTML, (modal) => {
+    modal.querySelector('button[name="close"]')?.addEventListener('click', () => modal.close());
+  });
+  customModal.showModal();
+};
+
 /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
@@ -808,6 +823,7 @@ async function loadLazy(doc) {
   const sk = document.querySelector('helix-sidekick');
   if (sk) {
     sk.addEventListener('custom:preflight', preflightListener);
+    sk.addEventListener('custom:publishlater', publishLaterListener);
     const publishButtons = sk.shadowRoot.querySelectorAll('button[title="Publish"]');
     publishButtons.forEach((publishButton) => {
       // eslint-disable-next-line func-names, consistent-return
