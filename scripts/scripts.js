a04ca0668d5e795f74a34ffcef8919cc4aaac966
Jaspher Dugang <jaspher.dugang@accenture.com>
2023-12-19 20:46:27 +0800

    Fix: remove delay from the loadDelayed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -910,18 +910,27 @@ async function loadSemiDelayed() {
  * Loads everything that happens a lot later,
  * without impacting the user experience.
  */
-function loadDelayed() {
+// function loadDelayed() {
+//   // article processing
+//   window.setTimeout(() => loadSemiDelayed(), 2000);
+//   // eslint-disable-next-line import/no-cycle
+//   window.setTimeout(() => import('./delayed.js'), 3000);
+//   // load anything that can be postponed to the latest here
+// }
+
+async function loadDelayed() {
   // article processing
-  window.setTimeout(() => loadSemiDelayed(), 2000);
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
+  await loadSemiDelayed();
+  const importDelayedJs = async () => {
+    await import('./delayed.js');
+  }
+  await importDelayedJs();
 }
 
 async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
-  loadDelayed();
+  await loadDelayed();
 }
 
 loadPage();
