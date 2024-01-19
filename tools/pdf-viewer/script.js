259bde8c8adc09254d77da2d70ea9e10d72a9920
Jaspher Dugang <jaspher.dugang@accenture.com>
2024-01-19 19:56:24 +0800

    Add conditions for PDF viewer

--- tools/pdf-viewer/script.js
+++ tools/pdf-viewer/script.js
@@ -1,2 +1,15 @@
-console.log('here');
-console.log(window.location.href);
+
+
+const pdfViewerHandler = () => {
+	const oUrlParams = new URLSearchParams(window.location.search);
+	const sUrlOrigin = window.location.origin;
+	const sPath = oUrlParams.get('path');
+	if (sPath) {
+		window.location.href = `${sUrlOrigin}${sPath}`;
+		return;
+	}
+	const oMessage = document.querySelector('.message');
+	oMessage.style.display = 'block';
+};
+
+pdfViewerHandler();
