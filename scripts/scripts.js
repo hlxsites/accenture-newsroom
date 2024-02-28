d263a41a020e8dc9f8c04d0b9ce127f769ff9e6b
jjjaspher <142872664+jjjaspher@users.noreply.github.com>
2024-02-28 10:44:40 +0800

    Get the latest from the main branch (#522)
    * Implement tag validation and publishing confirmation (#499)
    
    * Refactor: Implement Tag validation and Add info on confirmation pop up
    
    * Fix: refactor aside tag and publishing confirmation
    
    * Update: path getTaxonomy and fix lint errro
    
    * Update: path getTaxonomy
    
    * Update invalid tags error message
    
    * Update invalid tag error message
    
    ---------
    
    Co-authored-by: Jaspher Dugang <jaspher.dugang@accenture.com>
    
    * moved helixSideKickObserver (#515)
    
    Co-authored-by: Jaspher Dugang <jaspher.dugang@accenture.com>
    
    * update publish confirmation (#516)
    
    Co-authored-by: Jaspher Dugang <jaspher.dugang@accenture.com>
    
    * Update: Add condition in using csp.js file in prod (#518)
    
    * Update: Add condition in using csp.js file in prod
    
    * Fix: Linting error
    
    ---------
    
    Co-authored-by: Jaspher Dugang <jaspher.dugang@accenture.com>
    Co-authored-by: veejayricana <143703040+veejayricana@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,7 @@ import {
   ANALYTICS_LINK_TYPE_CONTENT_MODULE,
   ANALYTICS_MODULE_YEAR_FILTER,
   ANALYTICS_LINK_TYPE_FILTER,
+  INVALID_TAG_ERROR,
 } from './constants.js';
 import ffetch from './ffetch.js';
 import {
@@ -153,10 +154,18 @@ export function createTag(tag, attributes, html) {
   return el;
 }
 
+const isCloudFront = () => {
+  const sHostOrigin = window.location.origin;
+  return sHostOrigin.includes('https://newsroom');
+};
+
 /**
  * Sets the Content-Security-Policy meta tag to the document based on JSON file
  */
 async function setCSP() {
+  if (isCloudFront()) {
+    return;
+  }
   const resp = await fetch(`${window.hlx.codeBasePath}/scripts/csp.json`);
   const json = await resp.json();
   const directives = Object.keys(json);
@@ -804,6 +813,42 @@ const preflightListener = async () => {
   customModal.showModal();
 };
 
+const hasInvalidTags = () => {
+  const oAsideComponent = document.querySelector('.aside-container');
+  let bHasInvalidTags = false;
+  if (!oAsideComponent) {
+    return false;
+  }
+  const oIndustry = oAsideComponent.querySelector('.tags .industry');
+  const oSubject = oAsideComponent.querySelector('.tags .subject');
+  const aAllTagsText = [];
+  if (oIndustry) {
+    const oIndustryTags = oIndustry.querySelectorAll('.industry-tag a');
+    oIndustryTags.forEach((oLink) => aAllTagsText.push(oLink.innerText));
+  }
+  if (oSubject) {
+    const oSubjectTags = oSubject.querySelectorAll('.subject-tag a');
+    oSubjectTags.forEach((oLink) => aAllTagsText.push(oLink.innerText));
+  }
+
+  aAllTagsText.forEach((sTags) => {
+    if (sTags.includes(INVALID_TAG_ERROR)) {
+      bHasInvalidTags = true;
+    }
+  });
+  return bHasInvalidTags;
+};
+
+const getContentDate = () => {
+  const sPublishDate = document.querySelector('meta[name=publisheddate]').content || '';
+  const oDateObject = new Date(sPublishDate);
+  if (oDateObject.toDateString() === 'Invalid Date') {
+    return 'Invalid Date';
+  }
+  const extractedDate = sPublishDate.split(' ')[0];
+  return extractedDate;
+};
+
 // Set event for the publish button for confirmation message
 const publishConfirmationPopUp = (oPublishButtons) => {
   const oSidekick = document.querySelector('helix-sidekick');
@@ -814,14 +859,19 @@ const publishConfirmationPopUp = (oPublishButtons) => {
   oPublishButtons.forEach((oPublishBtn) => {
     // eslint-disable-next-line func-names, consistent-return
     oPublishBtn.addEventListener('mousedown', function (e) {
+      if (hasInvalidTags()) {
+        // eslint-disable-next-line no-alert
+        alert(`Publishing Error: Unable to publish page. Invalid tags detected. Please review and correct the tags before attempting to publish again.\nContent Date is ${getContentDate()}\n`);
+        e.stopImmediatePropagation();
+        return;
+      }
       // eslint-disable-next-line no-restricted-globals, no-alert
-      if (confirm('Are you sure you want to publish this content live?')) {
+      if (confirm(` Are you sure you want to publish this content live?\n Content Date is ${getContentDate()}`)) {
         // continue publishing
         this.click();
       } else {
         // avoid publishing
         e.stopImmediatePropagation();
-        return false;
       }
     });
   });
